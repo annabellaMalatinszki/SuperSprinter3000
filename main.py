8178a7cd100f1f830b2e0ebf9bb81a53cf19e78f
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-07-11 14:43:00 +0200

    Create stories.csv to store story data

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def get_story_list():
-    return render_template("list.html")
+    stories = data_manager.open_file("stories.csv")
+    return render_template("list.html", stories=stories)
 
 
 @app.route("/story")
 @app.route("/story/<story_id>")
-def get_story():
+def get_story(story_id=None):
+    if story_id:
+        stories = data_manager.open_file("stories.csv")
+        for story in stories:
+            if str(story_id) in story:
+                return render_template("form.html", story_id=story_id, story=story)
     return render_template("form.html")
 
 
