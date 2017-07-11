83ad8871e1bbcfa48489446f01d30749cd0159cc
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-07-11 14:20:17 +0200

    Add template-rendering functions to main.py

--- main.py
+++ main.py
@@ -4,5 +4,17 @@ from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
 
+@app.route("/")
+@app.route("/list")
+def get_story_list():
+    return render_template("list.html")
+
+
+@app.route("/story")
+@app.route("/story/<story_id>")
+def get_story():
+    return render_template("form.html")
+
+
 if __name__ == "__main__":
     main(debug=True)
